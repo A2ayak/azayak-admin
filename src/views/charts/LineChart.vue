0647f360bcf8bad4280536e40ba0dff5a76b9e3e
A2ayak <429853312@qq.com>
2024-02-22 16:27:54 +0800

    调整文件名

---
+++ src/views/charts/LineChart.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		asdasdasd
+		<svg-icon name="vue"></svg-icon>
+		<not-found-page></not-found-page>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref, onMounted } from 'vue'
+
+onMounted(() => {})
+</script>
+<style scoped lang="less"></style>
